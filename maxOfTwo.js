32faa93517048448f5dda4bac1797f56555b0980
Mason Lundquist <maslundq73@gmail.com>
2022-02-26 14:38:20 -0700

    Initial

---
+++ maxOfTwo.js
@@ -0,0 +1,18 @@
+let x = 1
+let y = 2
+
+// Write your code below this line.
+
+if (x > y) {
+    console.log(x)
+} else {
+console.log(y)
+}
+
+if (X > y) {
+    console.log(x)
+} else if (y > x) {
+    console.log(y)
+} else {
+    console.log ("They are the same")
+}
